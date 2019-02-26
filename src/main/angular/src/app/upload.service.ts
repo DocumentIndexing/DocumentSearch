d237288f890c49d1c6b65dc361990eeba46b8589
James Fawke <james.fawke@g4pas.co.uk>
2019-02-26 16:10:10 +0000

    Add logging

--- src/main/angular/src/app/upload.service.ts
+++ src/main/angular/src/app/upload.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
 import 'rxjs/add/operator/catch';
 
-  @Injectable()
-  export class UploadService {
+@Injectable()
+export class UploadService {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    send(data: any) {
-      console.log("we are hitting the send data")
-      return this.http.post('/index/upload', data);
-    }
+  send(data: any) {
+    console.log('we are hitting the sesnd data');
+    this.http.post<any>('/index/upload', data).subscribe(error => console.log(error.toString));
+  }
 }
